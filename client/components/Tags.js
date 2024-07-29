66e57d1d1cac817dbfc7785838d6435c3c598c81
임유진 <im706374@gmail.com>
2024-07-29 17:30:23 +0900

    fix | Tags.js 해시태그 모은 데이터에 맞게 수정

--- client/components/Tags.js
+++ client/components/Tags.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const tags = ['Fiction', 'Non-Fiction', 'Science', 'Fantasy', 'Biography', 'History', 'Children', 'Mystery', 'Romance', 'Thriller', 'Young Adult', 'Philosophy', 'Self-Help'];
+const tags = ['#전기', '#영어', '#예술', '#기독교', '#향수', '#청소', '#소설', '#AI', '#육아', '#자연', '#과학', '#요리', '#학습', '#야구', '#감성', '#여행', '#미스터리', '#추억', '#긴장감'];
 
 export default function Tags() {
   const handleTagClick = async (tag) => {
