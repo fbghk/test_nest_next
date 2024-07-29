90871a4338c260774c5601ce31760e8b8c76a675
임유진 <im706374@gmail.com>
2024-07-29 11:43:26 +0900

    초기 설정 진행

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,12 +1,31 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
 import SearchBar from '../components/SearchBar';
 import Tags from '../components/Tags';
 
 export default function Home() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <h1>Bookstore</h1>
-      <SearchBar />
-      <Tags />
+    <div className={styles.container}>
+      <Head>
+        <title>Bookstore</title>
+        <meta name="description" content="A simple bookstore app" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to Bookstore
+        </h1>
+        <p className={styles.description}>
+          Find your favorite books
+        </p>
+        <div className={styles.searchBar}>
+          <SearchBar />
+        </div>
+        <div className={styles.tags}>
+          <Tags />
+        </div>
+      </main>
     </div>
   );
 }
