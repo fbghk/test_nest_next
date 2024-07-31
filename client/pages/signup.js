191ac73c62e5480c7df56e11e2afeac7ee69a7db
임유진 <im706374@gmail.com>
2024-07-31 12:39:27 +0900

    New | signup.js 회원가입 페이지 생성

---
+++ client/pages/signup.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import styles from '@/Auth.module.css';
+
+export default function SignUp() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>#Hello #Welcome</h1>
+      <form className={styles.form}>
+        <label className={styles.label}>#Enter Your Email</label>
+        <input type="email" placeholder="User Id / Email" className={styles.input} />
+        <label className={styles.label}>#Create Your Password</label>
+        <input type="password" placeholder="Password" className={styles.input} />
+        <label className={styles.label}>#Enter Your Address</label>
+        <input type="text" placeholder="Address" className={styles.input} />
+        <label className={styles.label}>#Enter Your Number</label>
+        <input type="text" placeholder="Number" className={styles.input} />
+        <button type="submit" className={styles.button}>#SIGN UP</button>
+      </form>
+    </div>
+  );
+}
