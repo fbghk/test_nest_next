ab9bdd4c5cce8ac5f0bde4c838a5cd0c01f292d6
임유진 <im706374@gmail.com>
2024-07-31 12:38:54 +0900

    New | signin.js 로그인 페이지 생성

---
+++ client/pages/signin.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import styles from '@/Auth.module.css';
+
+export default function SignIn() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>#Welcome #Back</h1>
+      <form className={styles.form}>
+        <input type="text" placeholder="User Id / Email" className={styles.input} />
+        <input type="password" placeholder="Password" className={styles.input} />
+        <button type="submit" className={styles.button}>#SIGN IN</button>
+      </form>
+      <div className={styles.links}>
+        <a href="#" className={styles.link}>#Forgot Password?</a>
+        <a href="/signup" className={styles.link}>#Create Your Account</a>
+      </div>
+    </div>
+  );
+}
